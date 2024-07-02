336f386968fe8c4989d41e9318011744fd7afca6
natnaelAbebaw <natnaell70a@gmail.com>
2024-07-02 19:10:22 +0300

    modify the url to bbc weather

--- tests/weather.spec.js
+++ tests/weather.spec.js
@@ -1,109 +1,122 @@
 const { chromium } = require("playwright");
 const { test, expect } = require("@playwright/test");
 
-test("Weather.com search test", async ({ page }) => {
-  // Navigate to the website
-  await page.goto("https://weather.com/");
+test.describe("Weather Website", () => {
+  test("Weather.com search test", async ({ page }) => {
+    // Navigate to the website
+    await page.goto("https://www.bbc.com/weather");
 
-  // Search for weather in a specific city
-  await page.fill(
-    'input[placeholder="Search City or Zip Code"]',
-    "New York City, NY"
-  );
-  await page.press('input[placeholder="Search City or Zip Code"]', "Enter");
+    // Search for weather in a specific city
+    await page.fill(
+      'input[placeholder="Enter a city"]',
+      "Addis Ababa, Ethiopia"
+    );
 
-  await page.waitForSelector(".CurrentConditions--location--1XEyg");
+    // Interact with Elements (Click, Type)
+    await page.press('input[placeholder="Enter a city"]', "Enter");
 
-  // Verify the Results Using Assertions
-  const location = await page.$eval(
-    ".CurrentConditions--location--1YWj_",
-    (el) => el.textContent
-  );
-  expect(location).toContain("New York City, NY");
+    await page.click('a[data-id="344979"]');
 
-  const temperatureElement = await page.$(
-    ".CurrentConditions--tempValue--MHmYY"
-  );
-  expect(temperatureElement).not.toBeNull();
-  expect(await temperatureElement.isVisible()).toBe(true);
-});
+    await page.waitForSelector("#wr-forecast");
 
-(async () => {
-  //Navigate to the Website
-  const browser = await chromium.launch({ headless: true });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  await page.goto("https://weather.com/");
+    // Verify the Results Using Assertions
+    const location = await page.$eval(
+      "#wr-location-name-id",
+      (el) => el.textContent
+    );
+    expect(location).toContain("Addis Ababa");
 
-  // Handle Multiple Browser Contexts
-  const context2 = await browser.newContext();
-  const page2 = await context2.newPage();
-  await page2.goto("https://weather.com/");
-  await page2.fill(
-    'input[placeholder="Search City or Zip Code"]',
-    "Los Angeles"
-  );
-  await page2.press('input[placeholder="Search City or Zip Code"]', "Enter");
-
-  // Interact with Elements (Click, Type)
-  await page.click(".CurrentConditions--overlayBox--3z3Ha");
-
-  //Take a Screenshot of the Results
-  await page.screenshot({ path: "weather.png" });
-
-  //Generate a PDF Report
-  await page.pdf({ path: "weather.pdf", format: "A4" });
-
-  //Mock API Responses (For Controlled Testing)
-  await page.route("**/weather/today/l/*", (route) => {
-    route.fulfill({
-      status: 200,
-      contentType: "application/json",
-      body: JSON.stringify({ temperature: "72°F", condition: "Sunny" }),
-    });
+    const temperatureElement = await page.$(".wr-value--temperature--c");
+    expect(temperatureElement).not.toBeNull();
+    expect(await temperatureElement.isVisible()).toBe(true);
   });
 
-  // Extract Weather Information from the Site
-  const temperature = await page.$eval(
-    ".CurrentConditions--tempValue--MHmYY",
-    (el) => el.textContent
-  );
-  const condition = await page.$eval(
-    ".CurrentConditions--phraseValue--mZC_p",
-    (el) => el.textContent
-  );
+  test("Das", async () => {
+    // Handle Multiple Browser Contexts
+    const browser = await chromium.launch({ headless: true });
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://www.bbc.com/weather");
 
-  console.log(`Temperature: ${temperature}, Condition: ${condition}`);
+    const context2 = await browser.newContext();
+    const page2 = await context2.newPage();
+    await page2.goto("https://www.bbc.com");
 
-  // Handle Dynamic Content and Pagination
-  await page.goto("https://weather.com/news");
+    await page.screenshot({ path: "weather.png" });
 
-  let hasNextPage = true;
+    //Generate a PDF Report
+    await page.pdf({ path: "weather.pdf", format: "A4" });
 
-  while (hasNextPage) {
-    const weatherNews = await page.evaluate(() => {
-      const data = [];
-      document.querySelectorAll(".ListItem--listItem--25ojW").forEach((row) => {
-        const description = row.querySelector(
-          ".Ellipsis--ellipsis--3ADai"
-        ).innerText;
-        const time = row.querySelector(
-          ".CollectionMediaList--date--3d7ib"
-        ).innerText;
-        data.push({ description, time });
+    //Mock API Responses (For Controlled Testing)
+    await page.route("**/today/l/*", (route) => {
+      route.fulfill({
+        status: 200,
+        contentType: "application/json",
+        body: JSON.stringify({ temperature: "72°F", condition: "Sunny" }),
       });
-      return data;
     });
+    await page.fill(
+      'input[placeholder="Enter a city"]',
+      "Addis Ababa, Ethiopia"
+    );
+    // Extract Weather Information from the Site
+    await page.press('input[placeholder="Enter a city"]', "Enter");
 
-    console.log("Weather news:", weatherNews);
+    await page.click('a[data-id="344979"]');
 
-    const nextButton = await page.$("Pagination--nextButton--3L5q9");
-    if (nextButton) {
-      await nextButton.click();
-      // Implement Waiting Strategies and Handle Timeouts
-      await page.waitForTimeout(2000);
-    } else {
-      hasNextPage = false;
-    }
-  }
-})();
+    await page.waitForSelector("#wr-forecast");
+
+    // Verify the Results Using Assertions
+    const location = await page.$eval(
+      "#wr-location-name-id",
+      (el) => el.textContent
+    );
+    const temperature = await page.$eval(
+      ".wr-value--temperature--c",
+      (el) => el.textContent
+    );
+    const status = await page.$eval(
+      ".wr-day-temperature__low-label",
+      (el) => el.textContent
+    );
+
+    console.log(
+      `location:${location}\nTemperature: ${temperature}\nstatus:${status}`
+    );
+
+    // i didn't find pagination on bbc weather and the weather.com is not responding fast so i cant test it but it will work.
+    // Handle Dynamic Content and Pagination
+    // await page.goto("https://www.weather.com/news");
+
+    // let hasNextPage = true;
+
+    // while (hasNextPage) {
+    //   const weatherNews = await page.evaluate(() => {
+    //     const data = [];
+    //     document
+    //       .querySelectorAll(".ListItem--listItem--25ojW")
+    //       .forEach((row) => {
+    //         const description = row.querySelector(
+    //           ".Ellipsis--ellipsis--3ADai"
+    //         ).innerText;
+    //         const time = row.querySelector(
+    //           ".CollectionMediaList--date--3d7ib"
+    //         ).innerText;
+    //         data.push({ description, time });
+    //       });
+    //     return data;
+    //   });
+
+    //   console.log("Weather news:", weatherNews);
+
+    //   const nextButton = await page.$("Pagination--nextButton--3L5q9");
+    //   if (nextButton) {
+    //     await nextButton.click();
+    //     // Implement Waiting Strategies and Handle Timeouts
+    //     await page.waitForTimeout(2000);
+    //   } else {
+    //     hasNextPage = false;
+    //   }
+    // }
+  });
+});
